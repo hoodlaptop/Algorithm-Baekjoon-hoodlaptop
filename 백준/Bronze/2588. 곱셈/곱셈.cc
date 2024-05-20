43379059835fa8bb2a91b7a1d86c765ea31f59b1
hoodlaptop <165354489+hoodlaptop@users.noreply.github.com>
2024-05-20 23:14:44 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cin >> num1;
+	cin >> num2;
+
+	cout << num1 * (num2 % 10) << endl;
+	cout << num1 * ((num2/10) % 10) << endl;
+	cout << num1 * ((num2%1000) / 100) << endl;
+	cout << num1 * num2 << endl;
+
+	return 0;
+}
