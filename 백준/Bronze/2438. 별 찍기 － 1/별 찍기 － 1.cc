0a471beae43bd31be4a03d56808de75784caa4d6
hoodlaptop <165354489+hoodlaptop@users.noreply.github.com>
2024-08-06 20:43:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int input;
+	cin >> input;
+
+	for (int i = 0; i < input; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
