80f35d308278cecdadd6cf23650a037d55012772
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-03-06 09:48:18 +0900

    Refactor solution function to improve readability

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
-int solution(int n) {
+int solution(int n) 
+{
     int answer = 1;
     
-    while(1) {
+    while(1) 
+    {
         if(n % answer == 1) return answer;
         else answer++;
     }
