1969f87b64062268ebd3bdf12322bb8081dbaddf
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-03-23 09:35:08 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) 
+{
+    int idx1 = 0;
+    int idx2 = 0;
+
+    for (const string& word : goal) 
+    {
+        if (idx1 < cards1.size() && word == cards1[idx1])
+        {
+            idx1++;
+        }
+        else if (idx2 < cards2.size() && word == cards2[idx2]) 
+        {
+            idx2++;
+        }
+        else 
+        {
+            return "No";
+        }
+    }
+
+    return "Yes";
+}
