070bec6574318cfe9b8defe9f922e8d75d5cf5a9
Developing Giroro <165354489+hoodlaptop@users.noreply.github.com>
2026-03-30 09:40:16 +0900

    [level 1] Title: 기사단원의 무기, Time: 65.84 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int number, int limit, int power) 
+{
+    int answer = 0;
+
+    for (int i = 1; i <= number; ++i) 
+    {
+        int count = 0;
+        
+        for (int j = 1; j * j <= i; ++j) 
+        {
+            if (i % j == 0) 
+            {
+                if (j * j == i) 
+                {
+                    count += 1;
+                } 
+                else 
+                {
+                    count += 2;
+                }
+            }
+        }
+
+        if (count > limit) 
+        {
+            answer += power;
+        } 
+        else 
+        {
+            answer += count;
+        }
+    }
+
+    return answer;
+}
